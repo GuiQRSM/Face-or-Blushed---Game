5f38a261131501b9803f962bd0806364f3a8a87c
GuiQRSM <endbeob15@gmail.com>
2021-12-06 15:00:36 -0300

    It implements in the 'children: <Widget>[]' of the Column() class, plus a Padding() that receives the Image.asset() class to display an image in the body.

--- lib/MenuFrame.dart
+++ lib/MenuFrame.dart
@@ -25,7 +25,13 @@ class _MenuFrameState extends State<MenuFrame> {
                 padding: EdgeInsets.only(top: 18),
               child: Image.asset(
                 "pics/face_or_blushed_01.png",
-              height: 100,),
+              height: 150,),
+              ),
+              Padding(
+                  padding: EdgeInsets.only(top: 18),
+                child: Image.asset(
+                    "pics/botton_f_o_b.png",
+                height: 100,),
               ),
             ],
           ),
