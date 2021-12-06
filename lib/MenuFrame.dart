9eebbc90bd38b3cd427d20f8cac8e5ec8d448184
GuiQRSM <endbeob15@gmail.com>
2021-12-06 15:16:28 -0300

    Implements in the 'children: <Widget>[]' of the Column() class, the crossAxisAlignment and mainAxisAlignment attributes.

--- lib/MenuFrame.dart
+++ lib/MenuFrame.dart
@@ -20,18 +20,20 @@ class _MenuFrameState extends State<MenuFrame> {
         child: SingleChildScrollView(
           padding: EdgeInsets.all(32),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Padding(
-                padding: EdgeInsets.only(top: 18),
+                padding: EdgeInsets.only(top: 150),
               child: Image.asset(
                 "pics/face_or_blushed_01.png",
-              height: 150,),
+              ),
               ),
               Padding(
                   padding: EdgeInsets.only(top: 18),
                 child: Image.asset(
                     "pics/botton_f_o_b.png",
-                height: 100,),
+                ),
               ),
             ],
           ),
